d843cd369c6cfc2ec14c23d635bb02f00d16cff7
RobertCarroll93 <32339972+RobertCarroll93@users.noreply.github.com>
2017-11-24 00:22:08 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+/* https://www.w3schools.com/w3js/w3js_slideshow.asp */
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 3000); // Image changes every 3 seconds
+}
